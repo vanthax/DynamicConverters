b4f7eeac2b11b94548a604ea21c04901ea9218bc
Frankenstein <frankenstein@bitcoffee.de>
2022-08-31 18:05:43 +0200

    Added multi assembly support.

--- DynamicConverters/Converters/ConversionContext.cs
+++ DynamicConverters/Converters/ConversionContext.cs
@@ -6,6 +6,9 @@ namespace DynamicConverters.Converters
     internal class ConversionContext<T>
     {
         private T source;
+        private List<Type> converterTypes;
+        private Assembly[]? assemblies;
+        private string? nSpace;
 
         public static ConversionContext<V> From<V>(V source)
         {
@@ -15,40 +18,105 @@ namespace DynamicConverters.Converters
         public ConversionContext(T source)
         {
             this.source = source;
+            converterTypes = new List<Type>();
+        }
+
+        public ConversionContext<T> Use(Assembly[] assemblies)
+        {
+            this.assemblies = assemblies;
+            return this;
+        }
+
+        public ConversionContext<T> Use(string nSpace)
+        {
+            this.nSpace = nSpace;
+            return this;
         }
 
         public Converter<V> To<V>()
         {
-            var nSpace = "DynamicConverters.Converters";
-            var assembly = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsClass && x.Namespace == nSpace 
-            && x.GetCustomAttribute(typeof(ConversionSourceAttribute)) != null).ToList();
-
-            foreach (Type type in assembly)
+            //use current assembly if nothing else has been specified
+            if (assemblies == null)
             {
-                var attr = type.GetCustomAttribute(typeof(ConversionSourceAttribute));
+                assemblies = new Assembly[1];
+                assemblies[0] = Assembly.GetExecutingAssembly();
+            }
 
-                if(attr != null)
+            foreach (Assembly assembly in assemblies)
+            {
+                var types = assembly.GetTypes().Where(x => x.IsClass && x.GetCustomAttribute(typeof(ConversionSourceAttribute)) != null);
+
+                //restrict to specific namespaces
+                if (nSpace != null)
+                    types = types.Where(x => x.Namespace == nSpace);
+
+                types = types.ToList();
+
+                foreach (Type type in types)
                 {
-                    ConversionSourceAttribute conversionSourceAttr = (ConversionSourceAttribute)attr;
+                    var attr = type.GetCustomAttribute(typeof(ConversionSourceAttribute));
 
-                    if(typeof(T) == conversionSourceAttr.SourceType)
+                    if (attr != null)
                     {
-                        var constructedType = type.MakeGenericType(typeof(V));
-                        object? instantiatedType = Activator.CreateInstance(constructedType);
+                        ConversionSourceAttribute conversionSourceAttr = (ConversionSourceAttribute)attr;
 
-                        if(instantiatedType != null)
+                        if (typeof(T) == conversionSourceAttr.SourceType)
                         {
-                            var converter = (Converter<V>)instantiatedType;
-                            converter.SetSource(source);
-                            return converter;
+                            converterTypes.Add(type);
                         }
                     }
                 }
             }
 
-            throw new NotImplementedException($"Converter for target {typeof(V)} with source {source} has not been implemented.");
+            if (converterTypes == null)
+                throw new NotImplementedException($"No converter found for {source}.");
+
+            if(converterTypes.Count == 1)
+                return InstantiateConverter<V>(converterTypes[0]);
+
+            //Multiple converters for a single source may exist, but targets must be declared only once for each source.
+            Converter<V>? actualConverter = null;
+            foreach (Type converterType in converterTypes)
+            {
+                List<MethodInfo> methods = converterType.GetType().GetMethods().Where(x => x.ReturnType == typeof(T)).ToList();
+
+                foreach (MethodInfo method in methods)
+                {
+                    var attr = method.GetCustomAttribute(typeof(ConversionTargetAttribute));
+
+                    if (attr != null)
+                    {
+                        ConversionTargetAttribute conversionTargetAttr = (ConversionTargetAttribute)attr;
+
+                        if (conversionTargetAttr.TargetType == typeof(T))
+                        {
+                            if (actualConverter != null)
+                                throw new Exception($"Ambigous configuration! Multiple target methods for target {typeof(V)} with source {source} have been identified.");
+
+                            actualConverter = InstantiateConverter<V>(converterType);
+                        }
+                    }
+                }
+            }
+
+            if(actualConverter != null)
+                return actualConverter;
+
+            throw new NotImplementedException($"no converter method for target {typeof(V)} with source {source} has not been implemented.");
         }
 
+        private Converter<X> InstantiateConverter<X>(Type converterType)
+        {
+            var constructedType = converterType.MakeGenericType(typeof(X));
+            object? instantiatedType = Activator.CreateInstance(constructedType);
 
+            if (instantiatedType != null)
+            {
+                var converter = (Converter<X>)instantiatedType;
+                converter.SetSource(source);
+                return converter;
+            }
+            throw new Exception($"Error creating new instance of converter for source {source} and target {typeof(X)}");
+        }
     }
 }
